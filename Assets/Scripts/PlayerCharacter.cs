a10ec5037b30d30ad92a0f2cd1ab4b8cc61253e8
Robert Sandoval <Robert.Sandoval@iam.local>
2018-09-26 15:20:29 -0500

    WIP Movement

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -5,24 +5,58 @@ using UnityEngine;
 public class PlayerCharacter : MonoBehaviour {
 
     [SerializeField]
-    private int lives = 3; //PAscalCase or camelCase
+    private int lives = 3; //PascalCase or camelCase
 
     [SerializeField]
     private string name = "Mario";
 
+    [SerializeField]
+    private float jumpHeight = 5, speed = 5;
+
+    private bool hasKey;
+
+    [SerializeField]
+    private bool isOnGround;
+
+    private Rigidbody2D rigidBody2DInstance;
+    private float horizontalInput;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+
+        rigidBody2DInstance = GetComponent<Rigidbody2D>();
+
+        //We have to initialize our rigidbody variable!
+        rigidBody2DInstance.gravityScale = 5;
+
+        if (hasKey)
+        {
+            float openTimer = 5;
+            for (int i = 0; i < openTimer; i++)
+            {
+                Debug.Log(i);
+            }
+            Debug.Log("Open the door after " + " Seconds!");
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		            
-        Debug.Log("Hello.");
+	void Update ()
+    {
+        //transform.Translate does NOT use physics, rigidbody does
+        //transform.Translate(0, -0.01f, 0); 
+
+        GetInput();
+        Move();
 	}
+    private void GetInput()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+    }
 
     private void Move()
     {
-
+        rigidBody2DInstance.velocity = new Vector2(horizontalInput, 0);
     }
 }
